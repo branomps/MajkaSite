7a29304a7e6b3b80fcead27430d57c12e4af1d3c
Branomps <branomps@gmail.com>
2020-06-10 06:52:10 +0100

    make changes to nav for small devies

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -1,7 +1,7 @@
 let count = 0;
 function openNav() {
     if (count === 0){
-        document.getElementById("sidenavigation").style.width = "250px";
+        document.getElementById("sidenavigation").style.width = "230px";
         count += 1;
         //console.log("count is" + count);
     }else{
