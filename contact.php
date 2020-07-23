b1748d636fafecb24923ee05df602019e657c13c
Branomps <branomps@gmail.com>
2020-07-23 15:15:06 +0100

    Small changes over the site

--- contact.php
+++ contact.php
@@ -1,17 +1,16 @@
 <?php
-if(!empty($_POST["send"])) {
+
 	$name = $_POST["name"];
 	$email = $_POST["email"];
 	$phone = $_POST["phone"];
 	$message = $_POST["message"];
-
+    $content = $message;
     $toEmail = "branomps@azet.sk";
-    $subject = "Info" + $name;
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+    $subject = "Info " . $name;
+	$mailHeaders = "From: " . $name . " <". $email .">\r\n";
 	if(mail($toEmail, $subject, $content, $mailHeaders)) {
 	    $message = "Your contact information is received successfully.";
 	    $type = "success";
 	}
-}
-require_once "contact-view.php";
+require_once "contact.php";
 ?>
